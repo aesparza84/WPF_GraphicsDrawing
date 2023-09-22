f69607d607abbd939a0b9826e97ac2b43eaa7029
384290 <384290@colum.edu>
2023-09-21 21:03:00 -0500

    Added back stuff i did

--- WinForm_Drawing/WinForm_Drawing/Form1.cs
+++ WinForm_Drawing/WinForm_Drawing/Form1.cs
@@ -12,7 +12,7 @@ namespace WinForm_Drawing
         private void NewPaint(object sender, PaintEventArgs e)
         {
             Graphics graphics = e.Graphics;
-            
+
             Brush tealBrush = new SolidBrush(Color.Teal);
             Brush orangeBrush = new SolidBrush(Color.DarkOrange);
             Brush redBrush = new SolidBrush(Color.DarkRed);
@@ -37,7 +37,7 @@ namespace WinForm_Drawing
             int screenWisth = this.Width;
             int screenHeight = this.Height;
 
-            Point centerPoint = new Point(screenWisth/2, screenHeight/2);
+            Point centerPoint = new Point(screenWisth / 2, screenHeight / 2);
 
             string menuTitle = "Super Omega Ultra";
 
@@ -46,21 +46,24 @@ namespace WinForm_Drawing
 
             graphics.DrawRectangle(penFour, new Rectangle(centerPoint.X, centerPoint.Y, 1, 1));
 
-            graphics.DrawEllipse(penFour, new Rectangle(screenWisth/2, screenHeight/2, 50, 50));
+            graphics.DrawEllipse(penFour, new Rectangle(screenWisth / 2, screenHeight / 2, 50, 50));
             graphics.DrawEllipse(thickColoredPen, new Rectangle(screenWisth / 2, screenHeight / 2, 50, 50));
 
-            
+
             graphics.DrawEllipse(penOne, new Rectangle(screenWisth / 2, 20, 20, 20));
             graphics.FillEllipse(redBrush, new Rectangle(screenWisth / 2, 20, 20, 20));
             //graphics.DrawArc(thickPen, new Rectangle(screenWisth/2, 20, 10, 10), 50f, 100f);
 
             //graphics.DrawEllipse(penOne, new Rectangle(screenWisth / 4, screenHeight/4, 20, 20));
-            graphics.DrawEllipse(penOne, new Rectangle((screenWisth / 4)+(screenWisth/2), screenHeight / 4, 20, 20));
-            graphics.DrawEllipse(penOne, new Rectangle((screenWisth / 4) + (screenWisth / 2), (screenHeight / 4)-(screenHeight/4), 20, 20));
+            //graphics.DrawEllipse(penOne, new Rectangle((screenWisth / 4)+(screenWisth/2), screenHeight / 4, 20, 20));
+            graphics.DrawEllipse(penOne, new Rectangle((screenWisth / 4) + (screenWisth / 2), (screenHeight / 4) - (screenHeight / 4), 20, 20));
             graphics.DrawEllipse(penOne, new Rectangle((screenWisth / 4), (screenHeight / 4) - (screenHeight / 4), 20, 20));
 
-            //int width = 
-            //graphics.DrawRectangle(penTwo, new Rectangle(screenWisth / 4, (screenHeight / 4) - (screenHeight / 4), ));
+            Point o = new Point((screenWisth / 4) + (screenWisth / 2), 0);
+            Point p = new Point((screenWisth / 4), (screenHeight / 4));
+            int width = o.X - p.X;
+            graphics.DrawRectangle(penTwo, new Rectangle((screenWisth / 4), 10, width, 150));
+            string f = "fdddddd";
         }
     }
 }
